7434ffd99dbb64edd8f06cd0bad3b3eac387139c
dnldl <dnldlscjf@gmail.com>
2024-02-25 20:44:23 +0900

    DAO 설정 완료

--- src/main/java/com/example/dao/CustomerDAO.java
+++ src/main/java/com/example/dao/CustomerDAO.java
@@ -1,4 +1,12 @@
 package com.example.dao;
 
-public class CustomerDAO {
+import com.example.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+    public void saveCustomer(Customer theCustomer);
+    public Customer getCustomer(int theId);
+    public void deleteCustomer(int theId);
 }
