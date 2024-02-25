8ee0b29b0146617abbfd693f05f51212e8f29143
dnldl <dnldlscjf@gmail.com>
2024-02-25 18:32:16 +0900

    다시 설정 완료

--- src/main/java/com/example/config/AppInitializer.java
+++ src/main/java/com/example/config/AppInitializer.java
@@ -1,4 +1,25 @@
 package com.example.config;
 
-public class AppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected  Class <?> [] getRootConfigClasses(){
+        return new Class[]{
+                AppContext.class
+        };
+    }
+    @Override
+    protected  Class <?> [] getServletConfigClasses(){
+        return new Class[]{
+                WebMvcConfig.class
+        };
+    }
+    @Override
+    protected String[] getServletMappings(){
+        return new String[]{
+                "/"
+        };
+    }
+
 }
