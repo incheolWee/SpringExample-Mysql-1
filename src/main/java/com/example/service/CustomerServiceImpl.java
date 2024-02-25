15f559d3b59dce1cc67ecc5e591cac48535f384e
dnldl <dnldlscjf@gmail.com>
2024-02-25 20:44:37 +0900

    Service 설정 완료

--- src/main/java/com/example/service/CustomerServiceImpl.java
+++ src/main/java/com/example/service/CustomerServiceImpl.java
@@ -1,34 +1,41 @@
 package com.example.service;
 
+
 import com.example.dao.CustomerDAO;
 import com.example.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
     @Autowired
     private CustomerDAO customerDAO;
 
     @Override
-    @Transactional //어떤어노테이션?
-    public List<Customer > getCustomers(){
+    @Transactional
+    public List<Customer> getCustomers() {
         return customerDAO.getCustomers();
     }
 
     @Override
     @Transactional
-    public Customer getCustomers(int theId){
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Override
+    @Transactional
+    public Customer getCustomer(int theId) {
         return customerDAO.getCustomer(theId);
     }
 
     @Override
     @Transactional
-    public void deletCustomer(int theId){
+    public void deleteCustomer(int theId) {
         customerDAO.deleteCustomer(theId);
     }
-
 }
