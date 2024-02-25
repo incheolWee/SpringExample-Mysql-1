8f95e3920e7cd83b1381258df7806af9010c4037
dnldl <dnldlscjf@gmail.com>
2024-02-25 18:47:27 +0900

    Entity 설정과 lombok 설정 완료

--- src/main/java/com/example/entity/Customer.java
+++ src/main/java/com/example/entity/Customer.java
@@ -1,4 +1,34 @@
 package com.example.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Override
+    public String toString() {
+        return  "Customer [id=" + id +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", email=" + email +
+                "]";
+    }
 }
